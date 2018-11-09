4d63a4af79070c7d18f064417a7dbc206fe1dde0
paul29m <m7paul29@gmailcom>
2018-11-09 02:45:48 +0200

    init spring config

---
+++ winewiki/apigateway/src/main/java/ro/fortech/winewiki/apigateway/init/ApigatewayApplication.java
@@ -0,0 +1,11 @@
+package ro.fortech.winewiki.apigateway.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayApplication.class, args);
+    }
+}
---
+++ winewiki/profile-micro/src/main/java/ro/fortech/winewiki/profilemicro/init/ProfileMicroApplication.java
@@ -0,0 +1,12 @@
+package ro.fortech.winewiki.profilemicro.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileMicroApplication.class, args);
+    }
+}

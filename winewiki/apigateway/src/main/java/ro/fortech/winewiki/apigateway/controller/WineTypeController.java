5483b7925e6711bd71312281d38d529d7ea40291
paul29m <m7paul29@gmailcom>
2018-11-10 12:17:01 +0200

    winetype update

--- winewiki/apigateway/src/main/java/ro/fortech/winewiki/apigateway/controller/WineTypeController.java
+++ winewiki/apigateway/src/main/java/ro/fortech/winewiki/apigateway/controller/WineTypeController.java
@@ -2,6 +2,8 @@ package ro.fortech.winewiki.apigateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ro.fortech.winewiki.apigateway.dto.ApiGatewayNewWineTypeDto;
+import ro.fortech.winewiki.apigateway.dto.ApiGatewayWineTypeDto;
 import ro.fortech.winewiki.apigateway.dto.ApiGatewayWineTypeDtoList;
 import ro.fortech.winewiki.apigateway.service.WineTypeService;
 
@@ -17,5 +19,39 @@ public class WineTypeController {
     public ApiGatewayWineTypeDtoList getAll() {
         ApiGatewayWineTypeDtoList wineTypeDtoList = new ApiGatewayWineTypeDtoList(wineTypeService.findAll());
         return wineTypeDtoList;
+    }
+    @RequestMapping (value = "getbyid", method = RequestMethod.POST)
+    public ApiGatewayWineTypeDto getById(@RequestBody Long id) {
+        return wineTypeService.findById(id);
+    }
+
+    @RequestMapping (value = "getbyid/{id}", method = RequestMethod.GET)
+    public ApiGatewayWineTypeDto getByIdFromUrl(@PathVariable Long id) {
+        return wineTypeService.findById(id);
+    }
+
+    @RequestMapping (value = "create", method = RequestMethod.POST)
+    public ApiGatewayNewWineTypeDto createPerson(@RequestBody ApiGatewayNewWineTypeDto newWineTypeDto) {
+        return wineTypeService.create(newWineTypeDto);
+    }
+
+    @RequestMapping (value = "update", method = RequestMethod.PUT)
+    public ApiGatewayWineTypeDto updatePerson(@RequestBody ApiGatewayWineTypeDto wineTypeDto) {
+        return wineTypeService.update(wineTypeDto);
+    }
+
+    @RequestMapping (value = "delete/{id}", method = RequestMethod.DELETE)
+    public void deletePerson(@PathVariable Long id) {
+        wineTypeService.delete(id);
+    }
+
+    @RequestMapping (value = "getbyname", method = RequestMethod.POST)
+    public ApiGatewayWineTypeDto getWineTypeByName(@RequestBody String name) {
+        return wineTypeService.findByName(name);
+    }
+
+    @RequestMapping (value = "getbyname/{name}", method = RequestMethod.GET)
+    public ApiGatewayWineTypeDto getWineTypeByNameFromUrl(@PathVariable String name) {
+        return wineTypeService.findByName(name);
     }
 }
--- winewiki/apigateway/src/main/java/ro/fortech/winewiki/apigateway/service/WineTypeServiceImpl.java
+++ winewiki/apigateway/src/main/java/ro/fortech/winewiki/apigateway/service/WineTypeServiceImpl.java
@@ -1,11 +1,15 @@
 package ro.fortech.winewiki.apigateway.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ro.fortech.winewiki.apigateway.dto.ApiGatewayNewWineTypeDto;
 import ro.fortech.winewiki.apigateway.dto.ApiGatewayWineTypeDto;
+import ro.fortech.winewiki.apigateway.mapper.NewWineTypeMapper;
 import ro.fortech.winewiki.apigateway.mapper.WineTypeMapper;
+import ro.fortech.winewiki.profilemicro.dto.NewWineTypeDto;
 import ro.fortech.winewiki.profilemicro.dto.WineTypeDto;
 import ro.fortech.winewiki.profilemicro.dto.WineTypeDtoList;
 
@@ -21,15 +25,22 @@ public class WineTypeServiceImpl implements WineTypeService {
     @Autowired
     private WineTypeMapper wineTypeMapper;
 
+    @Autowired
+    private NewWineTypeMapper newWineTypeMapper;
+
+
     @Override
-    public ApiGatewayNewWineTypeDto create(ApiGatewayNewWineTypeDto wineTypeDto) {
-        return null;
+    public ApiGatewayNewWineTypeDto create(ApiGatewayNewWineTypeDto newPersonDto) {
+        NewWineTypeDto dto = restTemplate
+                .postForEntity(wineTypeServiceUrl + "create", newWineTypeMapper.toInternal(newPersonDto), NewWineTypeDto.class).getBody();
+        return newWineTypeMapper.toExternal(dto);
     }
 
     @Override
     public void delete(Long id) {
-
+        restTemplate.delete(this.wineTypeServiceUrl + "delete/" + id);
     }
+
 
     @Override
     public Set<ApiGatewayWineTypeDto> findAll() {
@@ -51,16 +62,21 @@ public class WineTypeServiceImpl implements WineTypeService {
 
     @Override
     public ApiGatewayWineTypeDto update(ApiGatewayWineTypeDto wineTypeDto) {
-        return null;
+        HttpEntity<ApiGatewayWineTypeDto> updatedInstance = new HttpEntity<>(wineTypeDto);
+        ApiGatewayWineTypeDto apiGatewayWineTypeDto = wineTypeMapper.toExternal(
+                restTemplate.exchange(this.wineTypeServiceUrl + "update", HttpMethod.PUT, updatedInstance, WineTypeDto.class).getBody());
+        return apiGatewayWineTypeDto;
     }
 
     @Override
     public ApiGatewayWineTypeDto findById(Long id) {
-        return null;
+        WineTypeDto dto = restTemplate.postForEntity(this.wineTypeServiceUrl + "getbyid", id, WineTypeDto.class).getBody();
+        return wineTypeMapper.toExternal(dto);
     }
 
     @Override
     public ApiGatewayWineTypeDto findByName(String name) {
-        return null;
+        WineTypeDto dto = restTemplate.postForEntity(this.wineTypeServiceUrl + "getbyname", name, WineTypeDto.class).getBody();
+        return wineTypeMapper.toExternal(dto);
     }
 }
